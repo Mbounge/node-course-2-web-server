4c5a6f767d8a34122822881a97b88e0e3651c0e6
Mbongeni Ndlovu <Mbounge@Mbongenis-MacBook-Pro.local>
2019-02-02 18:10:10 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express(); // in order to make the app all we have to do is call the method express
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+// going to use middleware, lets you configure how your express app works, if it doesnt know hoe to do something you want 
+// we use express to read a static dir
+// app.use is how you register middleware and it takes a function
+app.use(express.static(__dirname + '/public')); // takes absolute path to the folder you want to serve up
+
+//  use next to tell express when were done, when dealing asyncronous functions
+// if you dont use next() your handlers will never fire
+// req.method gives you the http method (get, put etc) 
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`
+    
+    console.log(log);
+    fs.appendFile('server.log', log + '/n', (err) => { // callback function
+        if (err) {
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+});
+
+app.use((req, res, next) => {
+    res.render('maintenance.hbs')
+}); // will stop everything below it from rendering
+
+// we can start to set up our http route handlers
+
+// frist arg is root of app or url, second arg is for the function to run, the function that tells express what to send back to the person who sent the request
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();                 
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+// Handler registratioin 
+//res.send('<h1>Hello Express!<h1>') // response for http request
+// req stores info about all info for request coming in, headers, body etc, res is methods called the handler
+app.get('/', (req, res) => {  
+    res.render('home.hbs', { // render is used to make a different page
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to the home page',
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Unable to fulfill request'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on Port 3000');
+}); // its going to bind our app to a port on our machine
